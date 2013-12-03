873b2b701a264f7dc7ce954209f8e9afb4008cc8
trodevel <trodevel@gmail.com>
2013-12-04 00:59:49 +0100

    added source folder

---
+++ src/fsm/namespace_fsm.h
@@ -0,0 +1,11 @@
+/*
+ * $Id$
+ */
+
+#ifndef NAMESPACE_FSM_H
+#define NAMESPACE_FSM_H
+
+#define NAMESPACE_FSM_START  namespace fsm {
+#define NAMESPACE_FSM_END    }
+
+#endif  // NAMESPACE_FSM_H
---
+++ src/utils/types.h
@@ -0,0 +1,13 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef char int8;
+typedef char unsigned uint8;
+typedef short int16;
+typedef short unsigned uint16;
+typedef int int32;
+typedef unsigned int uint32;
+typedef long long int int64;
+typedef long long unsigned int uint64;
+
+#endif // TYPES_H
