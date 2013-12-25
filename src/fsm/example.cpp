5a3216d0275c942b40e2da6cc96161678d6397a5
trodevel <trodevel@gmail.com>
2013-12-26 00:36:22 +0100

    added usage of Loader.

--- src/fsm/example.cpp
+++ src/fsm/example.cpp
@@ -1,10 +1,44 @@
 #include "loader.h"
 
 #include <cstdio>
+#include <string>
 
-int main()
+int main( int argc, const char** argv )
 {
     printf( "Hello, world!\n" );
+
+    if( argc <= 1 )
+    {
+        printf( "USAGE: example <name.fsm>\n" );
+
+        return 0;
+    }
+
+    std::string filename( argv[1] );
+
+    fsm::Loader l;
+
+    printf( "reading %s ...\n", filename.c_str() );
+
+    bool b = l.load_file( filename );
+
+    if( !b )
+    {
+        printf( "ERROR: error loading file\n" );
+
+        return 0;
+    }
+
+    if( !l.is_script_read() )
+    {
+        printf( "ERROR: script is incomplete\n" );
+
+        return 0;
+    }
+
+    const std::vector<fsm::InputLine> &scr   = l.get_script();
+
+    printf( "done, %d lines read\n", scr.size() );
 
     return 0;
 }
