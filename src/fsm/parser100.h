7656070c385f4c76849d6dda5a62ece4526ddf11
trodevel <trodevel@gmail.com>
2013-12-29 01:34:10 +0100

    parse() gets a vector of InputLine-s.

--- src/fsm/parser100.h
+++ src/fsm/parser100.h
@@ -20,9 +20,28 @@ private:
     Parser100& operator=( const Parser100 & h ); // no copy
 
 public:
-    bool parse();
+    bool parse( const std::vector<InputLine> & v );
 
 private:
+    bool is_cmd_supported( const std::string & cmd );
+
+    void handle_action( const InputLine & l );
+    void handle_const( const InputLine & l );
+    void handle_finitestatemachine( const InputLine & l );
+    void handle_state( const InputLine & l );
+    void handle_signal( const InputLine & l );
+    void handle_nextstate( const InputLine & l );
+    void handle_sendsignal( const InputLine & l );
+    void handle_exit( const InputLine & l );
+    void handle_end( const InputLine & l );
+
+    void complete_current_state();
+    void complete_current_signal();
+
+
+    void throw_error( const std::string & msg, const InputLine & l );
+    void throw_error( const char* msg, const InputLine & l );
+
 
 private:
     Fsm   & h_;
