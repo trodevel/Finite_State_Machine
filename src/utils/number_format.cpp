b59819fef38d3f07b7078b69d017a577b7e58496
trodevel <trodevel@gmail.com>
2014-01-11 01:23:05 +0100

    added number type detection funtions

---
+++ src/utils/number_format.cpp
@@ -0,0 +1,49 @@
+// E110
+
+#include "number_format.h"  // self
+
+bool is_number_int( const std::string & arg )
+{
+    int sign_corr = 0;
+
+    // check if sign is present
+    if( arg.compare( 0, 1, "-" ))
+    {
+        sign_corr = 1;
+    }
+
+    // check if hex number
+    if( arg.compare( 0 + sign_corr, 2, "0x" ) )
+    {
+        for( int i = 2 + sign_corr; i < ( int )arg.length(); ++i )
+            {
+                int c = arg.at( i );
+                if( ( c < '0' || c > '9' ) && ( c < 'a' || c > 'f' ) )
+                    return false;
+            }
+    }
+    else
+    {
+        // check if a decimal number
+        for( int i = 0 + sign_corr; i < ( int )arg.length(); ++i )
+        {
+            int c = arg.at( i );
+            if( c < '0' || c > '9')
+                return false;
+        }
+    }
+    return true;
+}
+
+bool is_number_float( const std::string & arg )
+{
+    // check if a floating point number
+    for( int i = 0; i < ( int )arg.length(); ++i )
+    {
+        int c = arg.at( i );
+        if( c != '-' && c != '.' && ( c < '0' || c > '9' ) )
+            return false;
+    }
+
+    return true;
+}
---
+++ src/utils/number_format.h
@@ -0,0 +1,11 @@
+// E110
+
+#ifndef NUMBER_FORMAT_H
+#define NUMBER_FORMAT_H
+
+#include <string>
+
+bool is_number_int( const std::string & arg );
+bool is_number_float( const std::string & arg );
+
+#endif // NUMBER_FORMAT_H
