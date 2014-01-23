bcd5ab2edddaf30530b55e3f7afe987bdfb88c59
trodevel <trodevel@gmail.com>
2014-01-23 01:10:26 +0100

    added functions to define numerical and string constants.

--- src/fsm/fsm.cpp
+++ src/fsm/fsm.cpp
@@ -42,5 +42,38 @@ bool Fsm::clock()
     return false;
 }
 
+
+/**
+ * @return true if given constant already exist
+ */
+bool Fsm::has_const( const std::string & s ) const
+{
+    if( const_str_.count( s ) )
+        return true;
+    if( const_int_.count( s ) )
+        return true;
+    if( const_double_.count( s ) )
+        return true;
+
+    return false;
+}
+
+bool Fsm::add_const( const std::string & s, const std::string & v )
+{
+    const_str_[ s ] = v;
+    return true;
+}
+bool Fsm::add_const( const std::string & s, int32 val )
+{
+    const_int_[ s ] = val;
+    return true;
+}
+bool Fsm::add_const( const std::string & s, double val )
+{
+    const_double_[ s ] = val;
+    return true;
+}
+
+
 NAMESPACE_FSM_END
 
--- src/fsm/fsm.h
+++ src/fsm/fsm.h
@@ -2,10 +2,18 @@
 
 #include "fsm_i.h"    // FsmI
 
+#include <map>          // std::map
+#include "../utils/types.h" // int32
+
 NAMESPACE_FSM_START
 
 class Fsm: virtual public FsmI
 {
+public:
+    typedef std::map< std::string, std::string >    MapStrToStr;
+    typedef std::map< std::string, int32 >         MapStrToInt;
+    typedef std::map< std::string, double >         MapStrToDouble;
+
 public:
 
     Fsm();
@@ -21,6 +29,19 @@ public:
     bool start();
 
     bool clock();
+
+
+    // loading helpers
+    bool has_const( const std::string & s ) const;
+    bool add_const( const std::string & s, const std::string & v );
+    bool add_const( const std::string & s, int32 val );
+    bool add_const( const std::string & s, double val );
+
+private:
+    MapStrToStr     const_str_;
+    MapStrToInt     const_int_;
+    MapStrToDouble  const_double_;
+
 
 };
 
