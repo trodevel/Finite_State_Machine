c3d15e40c006f854d85f4bcab2b0b58f5c9855a6
trodevel <trodevel@gmail.com>
2014-01-31 01:17:57 +0100

    added dummy example.

---
+++ src/utils/example.cpp
@@ -0,0 +1,14 @@
+// dummy test file
+// E130
+
+#include "number_format.h"
+
+#include <cstdio>
+#include <string>
+
+int main( int argc, const char** argv )
+{
+    printf( "Hello, world!\n" );
+
+    return 0;
+}
