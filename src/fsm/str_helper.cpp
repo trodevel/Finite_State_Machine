e0d6fd51f3838042d326d141ded8471ef29c7eab
trodevel <trodevel@gmail.com>
2014-03-01 01:25:13 +0100

    added output of start state for Fsm

--- src/fsm/str_helper.cpp
+++ src/fsm/str_helper.cpp
@@ -65,6 +65,7 @@ std::string StrHelper::to_string( const Fsm & l )
     std::ostringstream s;
 
     s << "fsm\n"
+            << "start state = " << l.start_state_ << "\n"
             << "states = " << l.states_.size() << "\n"
             << to_string2( l.states_ )
             << "\n";
